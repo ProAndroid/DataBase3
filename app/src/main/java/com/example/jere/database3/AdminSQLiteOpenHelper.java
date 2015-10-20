29a8ad9939306f6ac98f348fd790e68a74701ab9
facuferreyralpv <facuferreyralp@gmail.com>
2015-10-20 09:59:47 -0300

    creados los metodos oncreate y onUpgrade

--- app/src/main/java/com/example/jere/database3/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/jere/database3/AdminSQLiteOpenHelper.java
@@ -1,5 +1,6 @@
 package com.example.jere.database3;
 
+import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -7,13 +8,30 @@ import android.database.sqlite.SQLiteOpenHelper;
  * Created by jere on 20/10/15.
  */
 public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    //public AdminSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+      //  super(context, name, factory, version);
+        private static final int DATABASE_VERSION=1;
+        private static final String DATABASE_NAME="Cabañas.db";
+        public AdminSQLiteOpenHelper(Context context ) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String CREATE_TABLE_CABAÑAS = "CREATE TABLE" + Cabaña.TABLE
+                + Cabaña.KEY_ID + "INTIGER PRIMARY KEY AUTOINCRIMENT,"
+                + Cabaña.KEY_name + "TEXT,"
+                + Cabaña.KEY_checkin + "TEXT,"
+                + Cabaña.KEY_checkout + "TEXT";
+        db.execSQL(CREATE_TABLE_CABAÑAS);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IS EXISTS"+ Cabaña.TABLE);
+        onCreate(db);
 
     }
 }

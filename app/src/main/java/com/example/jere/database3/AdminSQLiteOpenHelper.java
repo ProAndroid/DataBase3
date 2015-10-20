7e37001230d3f97deb7af2db167c374e3069a365
jereantequera <jerex999@gmail.com>
2015-10-20 08:36:35 -0300

    Cuerno petaso

---
+++ app/src/main/java/com/example/jere/database3/AdminSQLiteOpenHelper.java
@@ -0,0 +1,19 @@
+package com.example.jere.database3;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by jere on 20/10/15.
+ */
+public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/com/example/jere/database3/Cabaña.java
@@ -0,0 +1,8 @@
+package com.example.jere.database3;
+
+/**
+ * Created by jere on 20/10/15.
+ */
+public class Cabaña {
+    public final static String TABLE = "cabañas";
+}

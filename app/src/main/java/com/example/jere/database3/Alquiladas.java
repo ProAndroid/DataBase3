adfaeef9ad14b2bb5805e77ed8bec76dfb41cb88
facuferreyralpv <facuferreyralp@gmail.com>
2015-10-20 10:38:10 -0300

    cree la clase Alquiladas

---
+++ app/src/main/java/com/example/jere/database3/Alquiladas.java
@@ -0,0 +1,16 @@
+package com.example.jere.database3;
+
+/**
+ * Created by facu on 20/10/15.
+ */
+public class Alquiladas {
+     public final static String TABLE = "Alquiladas";
+     public static final String KEY_ID = "id";
+     public static final String KEY_checkin = "checkin";
+     public static final String KEY_checkout = "checkout";
+
+     public int cabaña_ID;
+     public String checkin;
+     public String checkout;
+
+}

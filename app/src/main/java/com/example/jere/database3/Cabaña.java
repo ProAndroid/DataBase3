af99932b589dc13295fd70c9487c081419fdb5e6
jereantequera <jerex999@gmail.com>
2015-10-20 09:07:31 -0300

    Creados todos las variables

--- app/src/main/java/com/example/jere/database3/Cabaña.java
+++ app/src/main/java/com/example/jere/database3/Cabaña.java
@@ -4,5 +4,16 @@ package com.example.jere.database3;
  * Created by jere on 20/10/15.
  */
 public class Cabaña {
+    //Nombre de la tabla
     public final static String TABLE = "cabañas";
+    //Set de valores prederterminados
+    public static final String KEY_ID = "id";
+    public static final String KEY_name = "name";
+    public static final String KEY_checkin = "checkin";
+    public static final String KEY_checkout = "checkout";
+
+    public int cabaña_ID;
+    public String name;
+    public String checkin;
+    public String checkout;
 }
